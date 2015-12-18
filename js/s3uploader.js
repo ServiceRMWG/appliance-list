440bc92410fa56476ae1f7b24b9ec47219ac25d2
refluster <refluster@gmail.com>
2015-12-18 22:04:39 +0900

    test worker

--- js/s3uploader.js
+++ js/s3uploader.js
@@ -1,7 +1,22 @@
 var S3Uploader = function() {
-	self.addEventListener('message', function(e) {
+	this.reqQueue = [];
+
+	self.addEventListener('message', this.msgHandler.bind(this), false);
+};
+
+S3Uploader.prototype.msgHandler = function(e) {
+	switch(e.data) {
+	case 'upload':
+		this.upload(e.data);
+		break;
+	case 'echo':
 		self.postMessage(e.data);
-	}, false);
+		break;
+	}
+};
+
+S3Uploader.prototype.upload = function(d) {
+	this.reqQueue.push(d);
 };
 
 new S3Uploader();
